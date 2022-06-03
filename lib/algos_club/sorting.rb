401108f61beda8f512b8d76ce3dfb08710086a14
Kevin Solorio <kevin.solorio@hey.com>
2022-06-03 13:10:49 -0500

    algos_club first session on sorting

--- lib/algos_club/sorting.rb
+++ lib/algos_club/sorting.rb
@@ -1,13 +1,61 @@
 module AlgosClub::Sorting
   extend self
 
-  def bubble_sort(collection)
-    return collection
+  def bubble_sort!(collection)
+		# init the flag for our while loop
+		still_swappin = true
+
+		# start loop, go until we haven't swapped any indexes
+    while still_swappin do
+			# start the loop assuming that everything is sorted
+      still_swappin = false
+			# go through indexes in the array
+			(collection.length - 1).times do |i|
+				# if the values of the indexes aren't in the correct order...
+				if collection[i] > collection[i + 1]
+					# swap 'em!
+					swap!(collection, i, i + 1)
+					# oops, it wasn't sorted, switch flag to true so tha we loop again
+					still_swappin = true
+				end
+			end
+		end
+
+		# if we've arrived here, it means we go through an entire loop
+		# without swapping, which means everything should be sorted
+	  return collection
+
   end
 
-  def selection_sort
+  def selection_sort!(collection)
+      results = []
+
+      collection.length.times do
+        min_i = min_index(collection)
+        results << collection[min_i]
+        collection.delete_at(min_i)
+      end
+
+    results
+  end
+
+  def insertion_sort
   end
 
   def swap!(collection, from_index, to_index)
+    tmp = collection[from_index]
+    collection[from_index] = collection[to_index]
+    collection[to_index] = tmp
   end
+
+  def min_index(collection)
+    min_index = 0
+    collection.each_with_index do |item, index|
+      if collection[min_index] > item
+        min_index = index
+      end
+    end
+    min_index
+  end
+
 end
--- spec/sorting_spec.rb
+++ spec/sorting_spec.rb
@@ -5,9 +5,41 @@ RSpec.describe "sorting algorithms" do
 
   describe "bubble_sort" do
     it "sorting an empty collection returns an empty collection" do
-      expect(AlgosClub::Sorting.bubble_sort([])).to be_empty
+      expect(AlgosClub::Sorting.bubble_sort!([])).to be_empty
     end
 
-    it "sorts the collection"
+    it "sorts the collection" do
+      expect(AlgosClub::Sorting.bubble_sort!(sample)).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+  end
+
+  describe "selection_sort" do
+    it "sorting an empty collection returns an empty collection" do
+      expect(AlgosClub::Sorting.selection_sort!([])).to be_empty
+    end
+
+    it "sorts the collection" do
+      expect(AlgosClub::Sorting.selection_sort!(sample)).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+  end
+
+  describe "swap!" do
+    let(:from) { 0 }
+    let(:to)   { 1 }
+
+    it "swaps position of two values in a collection" do
+      dupped_sample = sample.dup
+      AlgosClub::Sorting.swap!(dupped_sample, from, to)
+
+      expect(dupped_sample[from]).to equal sample[to]
+      expect(dupped_sample[to]).to equal sample[from]
+      expect(dupped_sample.length).to equal sample.length
+    end
+  end
+
+  describe "smallest_index" do
+    it "returns the index of the min value" do
+      expect(AlgosClub::Sorting.min_index(sample)).to be 4
+    end
   end
 end
