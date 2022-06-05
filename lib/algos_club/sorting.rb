a8de73c77284c9032c046ec5c427d7a0021ac379
Kevin Solorio <kevin.solorio@hey.com>
2022-06-05 11:52:59 -0500

    update tabs to spaces

--- lib/algos_club/sorting.rb
+++ lib/algos_club/sorting.rb
@@ -2,28 +2,28 @@ module AlgosClub::Sorting
   extend self
 
   def bubble_sort!(collection)
-		# init the flag for our while loop
-		still_swappin = true
+    # init the flag for our while loop
+    still_swappin = true
 
-		# start loop, go until we haven't swapped any indexes
+    # start loop, go until we haven't swapped any indexes
     while still_swappin do
-			# start the loop assuming that everything is sorted
+      # start the loop assuming that everything is sorted
       still_swappin = false
-			# go through indexes in the array
-			(collection.length - 1).times do |i|
-				# if the values of the indexes aren't in the correct order...
-				if collection[i] > collection[i + 1]
-					# swap 'em!
-					swap!(collection, i, i + 1)
-					# oops, it wasn't sorted, switch flag to true so tha we loop again
-					still_swappin = true
-				end
-			end
-		end
+      # go through indexes in the array
+      (collection.length - 1).times do |i|
+        # if the values of the indexes aren't in the correct order...
+        if collection[i] > collection[i + 1]
+          # swap 'em!
+          swap!(collection, i, i + 1)
+          # oops, it wasn't sorted, switch flag to true so tha we loop again
+          still_swappin = true
+        end
+      end
+    end
 
-		# if we've arrived here, it means we go through an entire loop
-		# without swapping, which means everything should be sorted
-	  return collection
+    # if we've arrived here, it means we go through an entire loop
+    # without swapping, which means everything should be sorted
+    return collection
 
   end
 
